b66cb4f723ffebd6595104187d6955bcce01b6e0
nick.li <nick.li@nicklide-MacBook-Pro.local>
2017-07-28 17:16:52 +0800

    echo linebot

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict"
+
+const line = require("@line/bot-sdk");
+const express = require("express");
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+};
+
+const client = new line.Client(config);
+
+const app = express();
+
+app.post("/webhook", line.middleware(config), (req, res)=>{
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((rst) => res.json(rst));
+});
+
+var handleEvent = (event)=>{
+  if(event.type !== "message" || event.message.type !== "text"){
+    return Promise.resolve(null);
+  }
+
+  const echo = {type: "text", text: event.message.text};
+
+  return client.replyMessage(event.replyToken, echo);
+};
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+  console.log("listening on "+port);
+});
